df9b86b7dcfb8e09f5b2e97e6d3686c68fd5147f
nicholas keyes <bubbzthegreat@gmail.com>
2024-04-30 13:42:08 -0500

    updated more description and pictures of LanguagePage

--- client/src/componentData/letterData.js
+++ client/src/componentData/letterData.js
@@ -5,8 +5,7 @@ const letterData = [
     number: "1",
     original: "Oath - אות",
     picture: "/images/letterImages/aleph.jpg",
-    description:
-      "Being the first letter / sign it initially represented a sign post",
+    description: `Being the first sign, "aleph" initially represented a sign post this is more accurately represented in the Hebrew Seals script where the sign clearly does not represent an ox.`,
   },
   {
     label: "Beith",
@@ -15,7 +14,7 @@ const letterData = [
     original: "Beith - בית",
     picture: "/images/letterImages/beith.jpg",
     description:
-      "בית (Beith) means house in the sense of a building, but also household; wife and children.",
+      "בית (Beith) means house in the sense of a home, a household; Elohim, father, wife and children.",
   },
   {
     label: "Gimel",
@@ -23,8 +22,8 @@ const letterData = [
     number: "3",
     original: "Gamal - גמל",
     picture: "/images/letterImages/gamal.jpg",
-    // description:
-    //   "The verb גמל (gamal) means to deal, or recompense in the sense of benefitting from. Derivation גמל (gamal) means camel. It is said that the letter reminds of a camel's neck.",
+    description:
+      `The verb גמל (gamal) in Hebrew signifies "to deal" or "to recompense," often in the context of benefiting from an action. The noun גמל (gamal) is associated with the camel. The letter's shape is sometimes said to resemble the neck of a camel.`,
   },
   {
     label: "Daleth",
@@ -33,18 +32,17 @@ const letterData = [
     original: "Dag - דג",
     picture: "/images/letterImages/dag.jpg",
     alternate: "",
-    description:
-      "	",
+    description: "initially representing a fish and now to represent the door of a tent.	",
   },
   {
     label: "Heh",
     title: "ה",
     number: "5",
     original: "Halal - הלל",
-    picture: "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTAyL3Jhd3BpeGVsX29mZmljZV8yNV9waG90b19vZl9zdW5fcmF5X2ZpbG1fYWVzdGhldGljX3JldHJvX21pbmltYV9jZDJhMGJmYy1mZWVjLTQxYTgtOWU2MC1kOTVkNmZmN2EzYTlfMS5qcGc.jpg",
-    alternate: "",
+    picture:
+      "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTAyL3Jhd3BpeGVsX29mZmljZV8yNV9waG90b19vZl9zdW5fcmF5X2ZpbG1fYWVzdGhldGljX3JldHJvX21pbmltYV9jZDJhMGJmYy1mZWVjLTQxYTgtOWU2MC1kOTVkNmZmN2EzYTlfMS5qcGc.jpg",
     description:
-      "The spelling and thus the meaning of this word is uncertain. Klein spells הא (he), meaning lo! behold! Fuerst holds to הי, and thinks it's a part of the name for heth; letter 8.",
+      `Originally, the Hebrew letter ה (Heh) was thought to symbolize rays of sunshine and is associated with the word הלל (halal), meaning "to shine." Additionally, ה (Heh) can also carry the meaning of "lo!" or "behold!" According to some interpretations.`,
   },
   {
     label: "Waw",
@@ -53,24 +51,25 @@ const letterData = [
     original: "Waw - וו",
     picture: "/images/letterImages/waw.webp",
     description:
-      "The word וו (waw) means hook or peg, and is strictly reserved for the hooks/ pegs that kept the curtains of the tabernacle in place.",
+      "The term וו (waw) denotes a hook or peg, specifically designated for the purpose of securing the curtains of the tabernacle. The letter ו (waw) is believed to resemble the shape of a hook or peg, reinforcing its association with this function",
   },
   {
     label: "Zayin",
     title: "ז",
     number: "7",
     original: "Zara - זרע",
-    picture: "https://media.istockphoto.com/id/971038924/vector/farmers-plows-the-land-with-animals.jpg?s=612x612&w=0&k=20&c=5-YdyDQoA7n4cyeA-0PJs6cD1HkimL_DuKUPX8PEJiA=",
+    picture:
+      "https://media.istockphoto.com/id/971038924/vector/farmers-plows-the-land-with-animals.jpg?s=612x612&w=0&k=20&c=5-YdyDQoA7n4cyeA-0PJs6cD1HkimL_DuKUPX8PEJiA=",
     description:
-      "he word זין does not occur in Scriptures. Klein suggests that the form of the zayin represents a hand weapon, and explains that zyn means arm, ornament, to arm, to adorn (no references to Scriptures).",
+      `The term זין does not appear in the Scriptures, but it symbolizes the farmer's plow, which is employed to sow the land with seed. This concept is derived from the noun זרע, signifying "seed," and the verb associated with sowing.`,
   },
   {
     label: "Chet",
     title: "ח",
     number: "8",
-    original: "Kargal - חרדל",
-    picture: "/images/letterImages/chet.jpg",
-    description: "chet represents a beetle",
+    original: "Choma - חומה",
+    picture: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpaintingvalley.com%2Fimages%2Fmoses-red-sea-painting-22.jpg&f=1&nofb=1&ipt=2678fa8d11757d9701ae14d7685500d944bea2f5e03458abaf820fa44faf240a&ipo=images",
+    description: `Wall of a city or a fortress. A true divider like stated in the parting of the ים סוף (yam suph) Exodus 14:22 "and they went the children of israel into the sea on dry ground and the waters to them "חומה" (a wall) on their right and left"`,
   },
   {
     label: "Tet",
@@ -79,7 +78,7 @@ const letterData = [
     original: "Tor - טור",
     picture: "",
     description:
-      "The origin of the teth is a bit of a mystery. Klein derives from טות (twh), spin, and renders teth to knot, knot together, to twist into each other, to interweave. The letter teth indeed looks like a little vortex or spiral.",
+      `"to twist into each other," or "to interweave." The appearance of the letter teth does resemble a small vortex or spiral, which aligns with these interpretations.`,
   },
   {
     label: "Yod",
@@ -88,23 +87,24 @@ const letterData = [
     original: "Yad - יד",
     picture: "/images/letterImages/yad.jpg",
     description:
-      "The word is synonymous with power or might; to fall in one's hands. It's typical that the alphabet's smallest letter came to mean power, but perhaps its shape reminded of a little fist. As postfix, this letter י (yod) forms a possessive, and as prefix it creates a third person singular imperfect.",
+      "The term is synonymous with power or might, signifying the idea of something falling into one's hands. It's intriguing that the smallest letter of the alphabet came to represent power, but perhaps its shape evoked the image of a small fist.",
   },
   {
     label: "Kaf",
     title: "כ",
     number: "20",
-    original: "kanaf - כנף",
-    picture: "",
+    original: "Kaf - כף",
+    picture: "https://parade.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MjAyMTAwODk3ODMwOTM3NjY4/phrases-to-ask-for-support-according-to-a-therapist.jpg",
     description:
-      "	One of two regular words for hand (the other being the 10th letter). The noun כף (kap) denotes the hand as outstretched, asking and weak. The word basically encompasses anything that is hollow or outstretched in order to receive something: dish, plate, etc. The letter kap is written ך when it occurs at the end of a word, and כ when it occurs at the beginning or half-way a word.",
+      "	The noun כף (kap) denotes the hand as outstretched, asking and weak. The word basically encompasses anything that is hollow or outstretched in order to receive something",
   },
   {
-    label: "",
+    label: "Lamed",
     title: "ל",
     number: "30",
     original: "Lamed",
-    picture: "https://media.bible.art/661717e0-add4-4b97-9de1-16693fdfdb2c-compressed.jpg",
+    picture:
+      "https://media.bible.art/661717e0-add4-4b97-9de1-16693fdfdb2c-compressed.jpg",
     description: `The verb למד (lamad) means learn or teach. Derivative תלמיד (talmid) means scholar (hence Talmud), and derivative מלמד means ox goad.`,
   },
   {
@@ -114,23 +114,24 @@ const letterData = [
     original: "Mayim - מים",
     picture: "/images/letterImages/mem.jpg",
     description:
-      " means waters in the sense of a larger body (sea, ocean). It is suggested that the letter mem looks like a wave.",
+      "Mem represents waters in the context of larger bodies such as seas or oceans. Some suggest that the letter mem resembles a wave, which aligns with its association with bodies of water.",
   },
   {
     label: "Nun",
     title: "נ",
     number: "50",
     original: "Nachash - נחש",
-    picture: "https://photographylife.com/wp-content/uploads/2021/04/eyelash-viper-is-very-beautiful-snakes-are-misunderstood-.jpg",
+    picture:
+      "https://photographylife.com/wp-content/uploads/2021/04/eyelash-viper-is-very-beautiful-snakes-are-misunderstood-.jpg",
     description:
-      "The verb נון (nun) means propagate, increase. Derivative נין means offspring, posterity.",
+      "Initially, the letter נ (Nun) represented a snake, a connection that is more apparent in Hebrew seals and ancient pictographs.",
   },
   {
     label: "Samech",
     title: "ס",
     number: "60",
     original: "Sabav - סבב",
-    picture: "",
+    picture: "https://images.pexels.com/photos/39279/ship-traffic-jams-rope-dew-cordage-39279.jpeg?auto=compress&cs=tinysrgb&w=600",
     description: `The verb סמך (samak) means lean upon, support, uphold. It is the verb that is used in the phrase "laying on of hands."`,
   },
   {
@@ -138,16 +139,18 @@ const letterData = [
     title: "ע",
     number: "70",
     original: "Ayin - עין",
-    picture: "https://www.shutterstock.com/image-photo/extreme-closeup-photo-african-womans-600nw-792868357.jpg",
+    picture:
+      "https://www.shutterstock.com/image-photo/extreme-closeup-photo-african-womans-600nw-792868357.jpg",
     description:
-      "The word עין (ayin) means eye in all regular senses, but also as means of expression (knowledge, character, etc.). The word עין (ayin) means spring or fountain. ",
+      `The word עין (ayin) indeed signifies "eye" in all regular senses, encompassing physical vision as well as metaphorical expressions like knowledge and character. Additionally, עין (ayin) carries the meaning of "spring" or "fountain.`,
   },
   {
     label: "Peh",
     title: "פ",
     number: "80",
-    original: "Panim - פנים",
-    picture: "https://t3.ftcdn.net/jpg/03/33/88/90/360_F_333889026_rP8UIHhS2HxzapSuYUVaW6tFxXwXbtpI.jpg",
+    original: "Peh - פה",
+    picture:
+      "https://t3.ftcdn.net/jpg/03/33/88/90/360_F_333889026_rP8UIHhS2HxzapSuYUVaW6tFxXwXbtpI.jpg",
     description:
       "The word פה (peh) means mouth, but is often synonymous with speech. With a little good will one may recognize a face with a mouth in the shape of this letter.",
   },
@@ -155,35 +158,41 @@ const letterData = [
     label: "Tsade",
     title: "צ",
     number: "90",
-    original: "tzamach - צמח",
-    picture: "https://www.360financialliteracy.org/var/ezflow_site/storage/images/360finlit/homepage-herospace-images/sprouting-plant/3313987-1-eng-US/sprouting-plant.jpg",
+    original: "tsadhe - צדי",
+    picture:
+      "https://www.360financialliteracy.org/var/ezflow_site/storage/images/360finlit/homepage-herospace-images/sprouting-plant/3313987-1-eng-US/sprouting-plant.jpg",
     description:
-      "Klein derives from the verb צוד (sud), to hunt, and states that צדי means fish hook (no Biblical occurrence). Another name for this letter is צדיק (saddiq), just, righteous, from the verb צדק (sadeq), to be just or righteous.",
+      "Klein derives from the verb צוד (sud), to hunt, and states that צדי means fish hook (no Biblical occurrence).",
   },
   {
     label: "Qoph",
     title: "ק",
     number: "100",
     original: "",
-    picture: "https://images.pexels.com/photos/544458/pexels-photo-544458.jpeg?auto=compress&cs=tinysrgb&w=600",
-    description: "This word occurs in Scriptures only as תקופה (tequpa), meaning a coming around, or circuit of space or time. Klein reports that the root verb קוף (qwp) covers a circular motion and that it also serves to denote the ear of an axe or needle, or the back of the head.",
+    picture:
+      "https://images.pexels.com/photos/544458/pexels-photo-544458.jpeg?auto=compress&cs=tinysrgb&w=600",
+    description:
+      "The root verb קוף (qwp) indeed encompasses the idea of circular motion. Furthermore, it is used to denote the ear of an axe or needle, or even the back of the head.",
   },
   {
     label: "Resh",
     title: "ר",
     number: "200",
     original: "R'ash - ראש",
-    picture: "https://images.pexels.com/photos/10786228/pexels-photo-10786228.jpeg?auto=compress&cs=tinysrgb&w=600",
-    description: "The very common word ראשׁ (rosh) basically means head, but is used to indicate whatever leads or comes first: captain, summit, cap stone.",
+    picture:
+      "https://images.pexels.com/photos/10786228/pexels-photo-10786228.jpeg?auto=compress&cs=tinysrgb&w=600",
+    description:
+      "The very common word ראשׁ (rosh) basically means head, but is used to indicate whatever leads or comes first: captain, summit, cap stone.",
   },
   {
     label: "Shen",
     title: "ש",
     number: "300",
     original: "Shen - שן",
-    picture: "https://wildlifephotosonline.co.uk/media/catalog/product/cache/7639c15f5bc1b3da150213f0b9d24e06/a/f/african-lion-0781-p.jpg",
+    picture:
+      "https://t4.ftcdn.net/jpg/06/29/93/19/360_F_629931975_W2Fu23XZpARfEjl4qbUj6OKdEBjEF3rI.jpg",
     description:
-      "As derivation from the verb שׁנן (shanan), sharpen, the word שׁן (shen) means tooth or ivory. Both the verb and the noun are used primarily in a literal sense: sharpening of swords and arrows, but sometimes figuratively: the sharpening of one's tongue (saying sharp, mean words) or the sharpening of one's mind (Deuteronomy 6:7). ",
+      `Derived from the verb שׁנן (shanan), meaning "to sharpen," the word שׁן (shen) signifies "tooth" or "ivory." also they are occasionally employed figuratively, as in the sharpening of one's mind, as in Deuteronomy 6:7.`,
   },
   {
     label: "Taw",
@@ -192,7 +201,7 @@ const letterData = [
     original: "Thaw - תו",
     picture: "",
     description:
-      "תו (taw) means mark, and its verb תוה (tawa), scribble, limit, is probably derived from the noun. ",
+   `תו (taw) does indeed signify "mark," and its verb תוה (tawa), meaning "to scribble" or "to limit," is likely derived from this noun. The more ancient form of this letter resembled an X, a shape that naturally lends itself to being a general mark.`,
   },
 ];
 export default letterData;

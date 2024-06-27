029864370cc28fd4a1fc61b87e97d5f5b7bcad82
nicholas keyes <bubbzthegreat@gmail.com>
2024-06-27 16:20:16 -0500

    updated DNA page for mobile media query

--- client/src/components/DNAHeader.jsx
+++ client/src/components/DNAHeader.jsx
@@ -169,63 +169,7 @@ function DNAHeader() {
           </div>
         </div>
       </div>
-      <div className="DNABase">
-        {/* <h1>Admixture</h1>
-        <div className="theBigNeanderthal">
-          <div className="neanderthalContainer">
-            <img
-              className="theNeanderthal"
-              src="https://i0.wp.com/www.labrujulaverde.com/en/wp-content/uploads/2024/02/neanderthal.jpg?fit=1300%2C1000&ssl=1"
-              alt="/public/images/DNA images/nanderhal.jpg"
-            />
-            <p>
-              Admixture in genetics refers to the process by which two or more
-              previously isolated populations begin interbreeding, resulting in
-              the introduction of new genetic lineages into a population. This
-              gene mixing significantly impacts the genetic makeup of the
-              resulting population, influencing traits, genetic diversity, and
-              evolutionary outcomes.
-            </p>
-            <p>
-              In modern times, admixture occurs through the interbreeding of
-              individuals from different backgrounds. For example, due to
-              colonization, a significant percentage of African Americans have
-              European DNA in their genomes. This pattern is also seen in other
-              populations that have experienced colonization and forced
-              interbreeding.
-            </p>
-            <p>
-              Modern science has concluded that Neanderthal and Denisovan DNA is
-              mixed into the modern human genome. However, there are no pure
-              Neanderthal, Denisovan, or other ancient lineages alive today.
-              Instead, their genetic material exists in small percentages within
-              our DNA.
-            </p>
-            <p>
-              As it is writtem in Thorah (Genesis 6:4), "The Nephilim ("The
-              Fallen") were on the earth in those days, and also afterward, when
-              the sons of Elohim came in to the daughters of men, and they bore
-              children to them. Those were the mighty men who were of old, men
-              of renown." This intermingling created various humanoid beings
-              with distinct DNA and lineage of "The Fallen." These genes live on
-              in varying percentages within us.
-            </p>
-            <p>
-              The flood described in religious texts is said to have eliminated
-              the pure lineages of "The Fallen." However, Noah, his three sons,
-              and their wives contained an admixture of these ancient genes.
-              Some of their descendants expressed these genes more than others. which is why after the flood the rephaim and children of Anak existed
-            </p>
-            <p>
-              Admixture is a fundamental process in genetics that shapes the
-              genetic structure of populations through the interbreeding of
-              distinct groups. It has significant implications for genetic
-              diversity, trait variation, health, and evolutionary history,
-              making it a crucial area of study in genetics, anthropology,
-              medicine, and conservation.
-            </p>
-          </div>
-        </div> */}
+      <div className="DNABase--Paragraph">
         <h3>Out of Africa Theory Debunked</h3>
         <p>
           The Out of Africa theory, also known as the Recent African Origin
@@ -250,7 +194,6 @@ function DNAHeader() {
           sons, forming distinct groups that populated various regions of the
           world.
         </p>
-
         <p>
           In summary, while the Out of Africa theory explains the global
           dispersion of modern humans from Africa based on genetic evidence, it
@@ -262,36 +205,42 @@ function DNAHeader() {
           observed in scientific studies, where each haplogroup has unique
           characteristics and cannot independently give rise to others.
         </p>
-        <div className="noahContainer">
-          <h3>Sons of Noach - בני נח</h3>
-          <div className="HaploContainer">{renderDNAHaploCard}</div>
-          <div className="disclaimer">
-            <h3>
-              *Disclaimer: These images do not represent all subclades of these
-              haplogroups.*
-            </h3>
+      </div>
+        <div className="HaploContainer">
+          <div className="h3Container">
+        <h3>Sons of Noach - בני נוח</h3>
           </div>
+          <div className="smallHaploContainer">
+          {renderDNAHaploCard}
+          </div>
+        <div className={`disclaimer--${context.color}`}>
+          <h3>
+            *Disclaimer: These images do not represent all subclades of these
+            haplogroups.*
+          </h3>
         </div>
-        <div className="TheFamilyTree">
-          <h1>The Family Tree - עץ המשפחה</h1>
-          <p>
-            This phylogenetic tree depicts the breakdown of haplogroups in
-            today's population. Claims regarding the lineage of Israel are not
-            based on fallacies or hypotheticals. The most logical approach is to
-            analyze each contemporary haplogroup, comparing and contrasting them
-            to identify which one aligns with the historical curses that were
-            foretold. These curses do not miss their mark, affecting the
-            survivors of the Trans-Atlantic and Trans-Saharan slave trade whose
-            ancestors and descendants are now scattered across the Earth. These
-            individuals have endured the loss of heritage, language, and
-            customs, often marginalized in every society they have been
-            dispersed to, as outlined in Deuteronomy chapter 28 of the Thorah.
-            It is not my position to tell other people who they are and who they
-            descend from, but with the help of YHWH (יהוה), we can all use the
-            tools of DNA and historical proof to come closer to recognizing who
-            we are. Bless His name forever and always, YHWH(יהוה).
-          </p>
         </div>
+      {/* <div className="noahContainer">
+      </div> */}
+      <div className={`theFamilyTree--${context.color}`}>
+        <h1>The Family Tree - עץ המשפחה</h1>
+        <p>
+          This phylogenetic tree depicts the breakdown of haplogroups in today's
+          population. Claims regarding the lineage of Israel are not based on
+          fallacies or hypotheticals. The most logical approach is to analyze
+          each contemporary haplogroup, comparing and contrasting them to
+          identify which one aligns with the historical curses that were
+          foretold. These curses do not miss their mark, affecting the survivors
+          of the Trans-Atlantic and Trans-Saharan slave trade whose ancestors
+          and descendants are now scattered across the Earth. These individuals
+          have endured the loss of heritage, language, and customs, often
+          marginalized in every society they have been dispersed to, as outlined
+          in Deuteronomy chapter 28 of the Thorah. It is not my position to tell
+          other people who they are and who they descend from, but with the help
+          of YHWH (יהוה), we can all use the tools of DNA and historical proof
+          to come closer to recognizing who we are. Bless His name forever and
+          always, YHWH(יהוה).
+        </p>
       </div>
     </div>
   );
